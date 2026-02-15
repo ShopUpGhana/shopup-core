794b0ffea63d8b16f4d9ee57b9d4d1920b899843
Andenis55 <aldensinnovations@gmail.com>
2026-02-14 18:33:16 -0800

    Add seller products bootstrap script

---
+++ js/bootstrap/seller-products.bootstrap.js
@@ -0,0 +1,23 @@
+// /js/bootstrap/seller-products.bootstrap.js
+(function () {
+  "use strict";
+
+  const c = window.ShopUpContainer;
+  if (!c) {
+    console.error("[ShopUp] ShopUpContainer not found.");
+    return;
+  }
+
+  c.register(
+    "sellerProductsController",
+    (cc) =>
+      window.ShopUpSellerProductsController.create({
+        productService: cc.resolve("productService"),
+        authService: cc.resolve("authService"),
+        logger: cc.resolve("logger"),
+      }),
+    { singleton: true }
+  );
+
+  c.resolve("sellerProductsController").start();
+})();
